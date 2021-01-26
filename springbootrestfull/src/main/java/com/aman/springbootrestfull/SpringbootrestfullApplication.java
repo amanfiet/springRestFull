05af50fb577dc91fb54e9763481e807a29b97879
Aman Kumar <amanfiet@gmail.com>
2021-01-26 10:20:18 +0530

    rest

--- springbootrestfull/src/main/java/com/aman/springbootrestfull/SpringbootrestfullApplication.java
+++ springbootrestfull/src/main/java/com/aman/springbootrestfull/SpringbootrestfullApplication.java
@@ -1,13 +1,28 @@
 package com.aman.springbootrestfull;
 
+import java.util.Date;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
+@RequestMapping("spring")
 public class SpringbootrestfullApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootrestfullApplication.class, args);
+	}
+	
+	@GetMapping("data")
+	public String welcome(){
+		
+		System.out.println("inside ");
+		
+		return new Date().toString();
 	}
 
 }
